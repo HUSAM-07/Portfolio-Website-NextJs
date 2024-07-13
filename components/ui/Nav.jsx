144a1338c3e894f7a018d50d7b5451900f67c6ab
Mohammed Husamuddin <uni.husam@gmail.com>
2024-07-13 15:01:23 +0530

    update

--- components/ui/Nav.jsx
+++ components/ui/Nav.jsx
@@ -34,7 +34,13 @@ const Nav = () => {
         <nav className="flex gap-8">
             {links.map((link, index)=> {
                 return ( 
-                    <Link href={link.path} key={index} className={''}>
+                    <Link
+                     href={link.path} 
+                     key={index} 
+                     className={`${
+                        link.path === pathname && "text-accent border-b-2 border-accent"
+                     } capitalize font-medium hover: text-accent transition-all`}
+                    >    
                      {link.name}
                     </Link>
                 );
