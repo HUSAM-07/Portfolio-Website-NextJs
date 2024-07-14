06bf8e58596b5f70c208bc50b0a0b8f7d2265f9b
Mohammed Husamuddin <uni.husam@gmail.com>
2024-07-14 11:21:09 +0530

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/ui/Header"
+import Header from "@/components/Header"
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrains_Mono = JetBrains_Mono({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
   variable: '--font-jetbrainsMono'
@@ -14,8 +16,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrains_Mono.variable}>{children}</body>
+      <body className={jetbrains_Mono.variable}>{children}
         <Header />
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
+        
+      </body>
     </html>
   );
 }
---
+++ app/resume/page.jsx
@@ -0,0 +1,8 @@
+
+export const Resume = () => {
+  return (
+    <div>resume page</div>
+  )
+};
+
+export default Resume;
